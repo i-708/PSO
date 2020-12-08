faf86fd14e5472a65bb5cfc5e9dfdb99776bd9f0
i-708 <naoya.inoue0830@gmail.com>
2020-12-08 23:53:38 +0900

    変数名などの修正

--- pso.py
+++ pso.py
@@ -12,16 +12,16 @@ class Particle:
     def __init__(self,N):
         self.n = N #粒子数
         self.x = np.array([[random.uniform(-5, 5)for i in range(self.n)], [random.uniform(-5, 5)for j in range(self.n)]]) #粒子位置
-        self.lbest = self.x #ローカルベスト
+        self.pbest = self.x #パーソナルベスト
         self.gbest = np.array([[float("inf")],[float("inf")]]) #グローバルベスト
-        self.w = 0.91 #慣性定数
+        self.w = 0.5 #慣性定数
         self.v = np.array([[0 for i in range(self.n)], [0 for j in range(self.n)]]) #粒子の速度
         self.r1 = 0.14 #乱数の最大値(1)
         self.r2 = 0.14 #乱数の最大値(2)
         self.xEvaluate = np.array([float("inf") for i in range(self.n)]) #現在位置の評価関数の値
-        self.lbestEvaluate = np.array([float("inf") for i in range(self.n)]) #ローカルベストの評価関数の値
+        self.pbestEvaluate = np.array([float("inf") for i in range(self.n)]) #パーソナルベストの評価関数の値
         #初期設定
-        self.lbestUpdate()
+        self.pbestUpdate()
         self.gbestUpdate()
 
     #粒子の位置を更新する関数
@@ -34,66 +34,53 @@ class Particle:
         r1 = np.array([[random.uniform(0, self.r1)] for i in range(self.n)])
         r2 = np.array([[random.uniform(0, self.r2)] for i in range(self.n)])
         #速度の計算
-        self.v = self.w * self.v + r1.T * (self.gbest - self.x) + r2.T * (self.lbest - self.x)
+        self.v = self.w * self.v + r1.T * (self.gbest - self.x) + r2.T * (self.pbest - self.x)
 
-    #ローカルベストの更新を行う関数
-    def lbestUpdate(self):
+    #パーソナルベストの更新を行う関数
+    def pbestUpdate(self):
         self.evaluationFunc()
-        #ローカルベストの更新
-        self.lbest = np.where(self.xEvaluate < self.lbestEvaluate, self.x,self.lbest)
+        #パーソナルベストの更新
+        self.lbest = np.where(self.xEvaluate < self.pbestEvaluate, self.x,self.pbest)
     
     #グローバルベストの更新を行う関数
     def gbestUpdate(self):
-        #最小値のローカルベストのインデックスを取得
-        index = np.argmin(self.lbestEvaluate)
+        #最小値のパーソナルベストのインデックスを取得
+        index = np.argmin(self.pbestEvaluate)
         #グローバルベストの更新
-        self.gbest=[[self.lbest[0][index] for i in range(self.n)],[self.lbest[1][index] for j in range(self.n)]]
+        self.gbest=[[self.pbest[0][index] for i in range(self.n)],[self.pbest[1][index] for j in range(self.n)]]
                 
     #評価関数の計算を行う関数
     def evaluationFunc(self):
         #評価関数：x^2+y^2
         self.xEvaluate = self.x[0,:] * self.x[0,:] + self.x[1,:] * self.x[1,:]
-        self.lbestEvaluate = self.lbest[0,:] * self.lbest[0,:] + self.lbest[1,:] * self.lbest[1,:]
+        self.pbestEvaluate = self.pbest[0,:] * self.pbest[0,:] + self.pbest[1,:] * self.pbest[1,:]
     
     #粒子の情報をすべて更新する関数
     def allUpdate(self):
         self.positionUpdate()
         self.velocityUpdate()
-        self.lbestUpdate()
+        self.pbestUpdate()
         self.gbestUpdate()
 
 start = time.time() #スタート時間
 
-N=10 #粒子数(1)
-M=10 #粒子数(2)
-L=10 #粒子数(3)
+N=30 #粒子数
 
 #粒子の設定
-x=Particle(N) 
-y=Particle(M)
-z=Particle(L)
+p = Particle(N)
 
-T = 1000 #打ち切り時刻
+T = 50 #打ち切り時刻
 
 #シミュレーション
 for t in range(T):
-    x.allUpdate()
-    y.allUpdate()
-    z.allUpdate()
-    xlist = x.x[0]
-    ylist = x.x[1]
-    plt.plot(xlist,ylist,".",c="pink")
-    xlist = z.x[0]
-    ylist = z.x[1]
-    plt.plot(xlist,ylist,".",c="blue")
-    xlist = y.x[0]
-    ylist = y.x[1]
-    plt.plot(xlist,ylist,".",c="black")
-    #plt.xlim(-5,5)
-    #plt.ylim(-5,5)
-    plt.plot(xlist,ylist,".")
+    p.allUpdate()
+    x = p.x[0]
+    y = p.x[1]
+    plt.plot(x,y,".",c="blue")
+    plt.xlim(-5,5)
+    plt.ylim(-5,5)
     plt.plot(0,0,"o",c="red")
-    plt.pause(0.00001)
+    plt.pause(0.05)
     plt.gca().clear()
 #シミュレーションにかかった時間の表示
 elapsed_time = time.time() - start
