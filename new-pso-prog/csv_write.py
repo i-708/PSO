38cd0f059d5d3d245f12aaa81073e3360de06d1e
i-708 <72598968+i-708@users.noreply.github.com>
2021-06-23 16:17:15 +0900

    csv_write.pyにディレクトリ生成機能を追加

--- new-pso-prog/csv_write.py
+++ new-pso-prog/csv_write.py
@@ -1,18 +1,55 @@
-def csv_writer():
-    import datetime
-    import new_pso
+import datetime
+import os
+import csv
+from new_pso import Particle
 
-    # 現在の日時を取得
-    now = datetime.datetime.now()
+class WriteData():
+    
+    def __init__(self):
+        # 現在の日時を取得
+        self.now = datetime.datetime.now()
 
-    # ファイル名の生成
-    file_name = now.strftime('%Y%m%d_%H%M%S') + '.csv'
+        # ディレクトリ名の生成
+        self.new_dir_name = self.now.strftime('%Y%m%d_%H%M%S')
 
-    # pathの生成
-    path = 'PSO/new-pso-prog/result/' + file_name
+        # pathの生成
+        self.dir_path = 'PSO/new-pso-prog/result/' + self.new_dir_name
+        
+        # ディレクトリの生成
+        os.mkdir(self.dir_path)
+    
+    def csv_writer(self,particles):
+        D = len(particles[0].x[0])
+        T_MAX = len(particles[0].x)
 
-    with open(path, mode='w') as f:
-        f.write(file_name)
+        x_label = []
+        v_label = []
+        
+        # xとvのlabelを生成
+        for i in range(D):
+            x_label_name = 'x' + str(i)
+            v_label_name = 'v' + str(i)
+            x_label.append(x_label_name)
+            v_label.append(v_label_name)
+        
+        # rowの名前を生成
+        row_name = ['T','particle_num'] + x_label + v_label + ['func_ans','pbest','gbest']
+        
+        # ファイル名の生成
+        file_name = 'result.csv'
 
+        # pathの生成
+        file_path = self.dir_path + '/' + file_name
 
-csv_writer()
+        # csvへ書き込み
+        with open(file_path, mode='w') as f:
+            writer = csv.writer(f)
+            
+            # 行の名前を書き込み
+            writer.writerow(row_name)
+            
+            
+
+# w = WriteData()
+
+# w.csv_writer()
